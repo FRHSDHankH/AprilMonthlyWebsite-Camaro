8a5a1d3c832443200a807209b8b36979a52b8b20
Hank <155669403+FRHSDHankH@users.noreply.github.com>
2025-05-04 00:40:04 +0000

    Website Finished Finally

--- script.js
+++ script.js
@@ -43,3 +43,29 @@ function close(generation) {
   footer.classList.remove('d-none');
   gen.classList.add('d-none');
 }
+
+let close1 = document.getElementById('closeGen1')
+let close2 = document.getElementById('closeGen2')
+let close3 = document.getElementById('closeGen3')
+let close4 = document.getElementById('closeGen4')
+let close5 = document.getElementById('closeGen5')
+let close6 = document.getElementById('closeGen6')
+
+close1.addEventListener('click', () => {
+  close('gen1')
+})
+close2.addEventListener('click', () => {
+  close('gen2')
+})
+close3.addEventListener('click', () => {
+  close('gen3')
+})
+close4.addEventListener('click', () => {
+  close('gen4')
+})
+close5.addEventListener('click', () => {
+  close('gen5')
+})
+close6.addEventListener('click', () => {
+  close('gen6')
+})
